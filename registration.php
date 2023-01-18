1d7e747a2ce694d3cf7c355b2852ab75ca8aa7da
josseve <joss.eve@free.fr>
2023-01-18 14:34:06 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -4,6 +4,6 @@ use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Underser_TranslationHelper',
+    'Josseve_TranslationHelper',
     __DIR__
 );
